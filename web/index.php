2b3eb808b0f97373f62946e7979332a30bdb60d7
DylanMacalast <dylanmacalast1@gmail.com>
2019-08-20 21:20:19 +0100

    changing

--- web/index.php
+++ web/index.php
@@ -10,6 +10,11 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
+});
+
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
