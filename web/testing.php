dc0606d77511bdb782188ee1c78815b23b8a0dc9
DylanMacalast <dylanmacalast1@gmail.com>
2019-08-22 21:52:40 +0100

    testing

--- web/testing.php
+++ web/testing.php
@@ -1,14 +1,23 @@
 <?php
 
-$db = parse_url(getenv("DATABASE_URL"));
+     $db = parse_url(getenv("DATABASE_URL"));
 
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+     $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+    
+     function run(){
+        var_dump($this->pdo);
+    
+        $query = $this->pdo->prepare('SELECT * FROM test_table');
+        $query->execute();
+        $query->fetchAll();
+    }
+   
 
-var_dump($pdo);
+
